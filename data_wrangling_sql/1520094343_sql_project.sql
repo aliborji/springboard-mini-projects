774abe2d7b3735dcca884927d5300481382825b7
Monil Gudhka <gudhkamonil@gmail.com>
2019-06-30 22:00:01 +0530

    Adding semi-colon for each query

--- data_wrangling_sql/1520094343_sql_project.sql
+++ data_wrangling_sql/1520094343_sql_project.sql
@@ -26,35 +26,37 @@ exploring the data, and getting acquainted with the 3 tables. */
 
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Please list the names of the facilities that do. */
-SELECT name FROM `Facilities` WHERE `membercost` != 0
+SELECT name FROM `Facilities` WHERE `membercost` != 0;
 
 
 /* Q2: How many facilities do not charge a fee to members? */
-SELECT count(*) FROM `Facilities` WHERE `membercost` = 0
+SELECT count(*) FROM `Facilities` WHERE `membercost` = 0;
 
 
 /* Q3: How can you produce a list of facilities that charge a fee to members,
 where the fee is less than 20% of the facility's monthly maintenance cost?
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
-SELECT `facid`, `name`, `membercost`, `monthlymaintenance` FROM `Facilities` WHERE `membercost` != 0 AND `membercost` < (`monthlymaintenance` * 0.2)
+SELECT `facid`, `name`, `membercost`, `monthlymaintenance` 
+FROM `Facilities` 
+WHERE `membercost` != 0 AND `membercost` < (`monthlymaintenance` * 0.2);
 
 
 /* Q4: How can you retrieve the details of facilities with ID 1 and 5?
 Write the query without using the OR operator. */
-SELECT * FROM `Facilities` WHERE `facid` IN (1,5)
+SELECT * FROM `Facilities` WHERE `facid` IN (1,5);
 
 
 /* Q5: How can you produce a list of facilities, with each labelled as
 'cheap' or 'expensive', depending on if their monthly maintenance cost is
 more than $100? Return the name and monthly maintenance of the facilities
 in question. */
-SELECT `name`, `monthlymaintenance`, IF(100<`monthlymaintenance`, "expensive", "cheap") as label FROM `Facilities`
+SELECT `name`, `monthlymaintenance`, IF(100<`monthlymaintenance`, "expensive", "cheap") as label FROM `Facilities`;
 
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Do not use the LIMIT clause for your solution. */
-SELECT `firstname`, `surname` FROM `Members` WHERE `joindate` = (SELECT max(`joindate`) FROM `Members`)
+SELECT `firstname`, `surname` FROM `Members` WHERE `joindate` = (SELECT max(`joindate`) FROM `Members`);
 
 
 /* Q7: How can you produce a list of all members who have used a tennis court?
@@ -66,7 +68,7 @@ FROM  `Members` m
 INNER JOIN  `Bookings` b ON b.`memid` = m.`memid` 
 INNER JOIN  `Facilities` f ON f.`facid` = b.`facid` 
 WHERE f.`name` LIKE  '%Tennis Court%'
-ORDER BY m.`firstname` 
+ORDER BY m.`firstname`;
 
 
 /* Q8: How can you produce a list of bookings on the day of 2012-09-14 which
@@ -80,7 +82,7 @@ INNER JOIN `Facilities` f ON f.`facid` = b.`facid`
 INNER JOIN  `Members` m ON b.`memid` = m.`memid` 
 WHERE b.`starttime` LIKE '2012-09-14%' 
 AND ((b.`memid` = 0 AND (b.`slots` * f.`guestcost`) > 30) OR (b.`memid` != 0 AND (b.`slots` * f.`membercost`) > 30))
-ORDER BY cost DESC
+ORDER BY cost DESC;
 
 
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
@@ -91,7 +93,7 @@ FROM (SELECT b.`memid`, f.`name`, IF(b.`memid`=0, b.`slots` * f.`guestcost`, b.`
     WHERE b.`starttime` LIKE '2012-09-14%' ) c
 INNER JOIN `Members` m ON m.`memid` = c.`memid`
 WHERE c.`cost` > 30
-ORDER BY c.`cost` DESC
+ORDER BY c.`cost` DESC;
 
 
 /* Q10: Produce a list of facilities with a total revenue less than 1000.
@@ -101,4 +103,4 @@ SELECT f.`name`, SUM(IF(b.`memid`=0, b.`slots` * f.`guestcost`, b.`slots` * f.`m
 FROM `Bookings` b
 INNER JOIN `Facilities` f ON f.`facid` = b.`facid`
 GROUP BY f.`name`
-ORDER BY revenue
+ORDER BY revenue;
