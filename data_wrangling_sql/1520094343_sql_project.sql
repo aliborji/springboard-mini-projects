28bdcdb4a4141d38e375301eaaa5c99b07ea5e4f
Monil Gudhka <gudhkamonil@gmail.com>
2019-06-30 19:44:33 +0530

    Answering 3 and 4 questions

--- data_wrangling_sql/1520094343_sql_project.sql
+++ data_wrangling_sql/1520094343_sql_project.sql
@@ -35,11 +35,11 @@ SELECT count(*) FROM `Facilities` WHERE `membercost` = 0
 where the fee is less than 20% of the facility's monthly maintenance cost?
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
-
+SELECT `facid`, `name`, `membercost`, `monthlymaintenance` FROM `Facilities` WHERE `membercost` != 0 AND `membercost` < (`monthlymaintenance` * 0.2)
 
 /* Q4: How can you retrieve the details of facilities with ID 1 and 5?
 Write the query without using the OR operator. */
-
+SELECT * FROM `Facilities` WHERE `facid` IN (1,5)
 
 /* Q5: How can you produce a list of facilities, with each labelled as
 'cheap' or 'expensive', depending on if their monthly maintenance cost is
